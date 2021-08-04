d2bf8822e94c937deaddba60bd8a7c7ca57101a9
Emily <emilylschultz@gmail.com>
2021-08-04 10:58:52 -0700

    Add code to compare individuals percent cover pixels over time

--- Code/Plots/PJcover_compare.R
+++ Code/Plots/PJcover_compare.R
@@ -4,6 +4,7 @@
 
 library(raster)
 library(RColorBrewer)
+library(tidyverse)
 
 # Set RAP percent cover data file pathway
 PC.path <-  "./PJCover/"
@@ -17,7 +18,7 @@ RAP <- stack(RAPFiles)
 
 # Select years of overlap
 names(PC) <- as.character(2000:2016)
-names(RAP) <- as.character(1984:2019)
+names(RAP) <- as.character(1984:2020)
 
 YearStart <- "X2000"
 YearEnd <- "X2016"
@@ -43,3 +44,74 @@ plot(d_PJcover, breaks=cuts, col=pal_div(11))
 for (i in 1:16){
 	plot(d_PJcover[[i]], breaks=cuts, col=pal_div(11)) 
 }
+
+
+# Compare individual pixels over time
+n <- 3
+max_y <- ncol(PC[[1]])
+max_x <- nrow(PC[[1]])
+
+y <- sample(1:max_y,n)
+x <- sample(1:max_x,n)
+
+PC_sample <- as.data.frame(PC[x,y])
+RAP_sample <- as.data.frame(RAP_subset[x,y])
+
+PC_sample$Sample<-rownames(PC_sample)
+RAP_sample$Sample<-rownames(RAP_sample)
+
+PC_sample_long <- pivot_longer(PC_sample,names(PC_sample)[-18],names_to = "Year", values_to = "PC")
+RAP_sample_long <- pivot_longer(RAP_sample,names(RAP_sample)[-18],names_to = "Year", values_to = "RAP")
+
+Sample <- merge(PC_sample_long, RAP_sample_long)
+
+Sample$Year <- as.numeric(str_sub(Sample$Year, 2,5))
+
+Sample <- pivot_longer(Sample,c(PC,RAP), names_to = "Dataset", values_to = "PC")
+
+mytheme<-theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+							 panel.background = element_blank(), axis.line = element_line(colour = "black"),
+							 legend.text=element_text(size=11),legend.title=element_text(size=12),
+							 legend.key = element_rect(fill = "white"),axis.text=element_text(size=12),
+							 axis.title.x=element_text(size=14),axis.title.y=element_text(size=14),
+							 axis.line.x = element_line(color="black", size = 0.3),
+							 axis.line.y = element_line(color="black", size = 0.3))
+
+sample1 <- ggplot(subset(Sample,Sample==1),aes(x=Year,y=PC,colour=Dataset)) +
+	geom_line(size=1.2) + mytheme
+
+sample2 <- ggplot(subset(Sample,Sample==2),aes(x=Year,y=PC,colour=Dataset)) +
+	geom_line(size=1.2) + mytheme
+
+sample3 <- ggplot(subset(Sample,Sample==3),aes(x=Year,y=PC,colour=Dataset)) +
+	geom_line(size=1.2) + mytheme
+
+sample4 <- ggplot(subset(Sample,Sample==4),aes(x=Year,y=PC,colour=Dataset)) +
+	geom_line(size=1.2) + mytheme
+
+sample5 <- ggplot(subset(Sample,Sample==5),aes(x=Year,y=PC,colour=Dataset)) +
+	geom_line(size=1.2) + mytheme
+
+sample6 <- ggplot(subset(Sample,Sample==6),aes(x=Year,y=PC,colour=Dataset)) +
+	geom_line(size=1.2) + mytheme
+
+sample7 <- ggplot(subset(Sample,Sample==7),aes(x=Year,y=PC,colour=Dataset)) +
+	geom_line(size=1.2) + mytheme
+
+sample8 <- ggplot(subset(Sample,Sample==8),aes(x=Year,y=PC,colour=Dataset)) +
+	geom_line(size=1.2) + mytheme
+
+sample9 <- ggplot(subset(Sample,Sample==9),aes(x=Year,y=PC,colour=Dataset)) +
+	geom_line(size=1.2) + mytheme
+
+sample1
+sample2
+sample3
+sample4
+sample5
+sample6
+sample7
+sample8
+sample9
+
+	
