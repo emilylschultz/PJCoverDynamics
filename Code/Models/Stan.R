000cf14fcdaf0371e72b1acf95170567ba3df070
Emily <emilylschultz@gmail.com>
2021-08-12 14:07:54 -0700

    Add code to make percent cover object a matrix

--- Code/Models/Stan.R
+++ Code/Models/Stan.R
@@ -9,14 +9,17 @@ library(rstan)
 PJdata <- read.csv("PJcover_data.csv")
 PJdataRAP.csv("PJcoverRAP_data.csv")
 
+load("./Output/PJcover_mat.rda")
+
+Location_data <- data.frame(location.x = location.x, location.y = location.y) %>%
+	mutate(Location=str_c(as.character(location.x),as.character(location.y))) %>%
+	mutate(Pixel_ID=as.numeric(factor(Location, levels = unique(Location))))
+
 # Remove data points with fire
 PJdata <- subset(PJdata,Fire==0) 
 PJdataRAP <- subset(PJdataRAP,Fire==0) 
 
-PJdata <- PJdata %>%
-	mutate(PJdata, Location=str_c(as.character(location.x),as.character(location.y))) %>%
-	arrange(Year_t,Location) %>%
-	mutate(Pixel_ID=as.numeric(factor(Location, levels = unique(Location))))
+PJdata <- merge(PJdata,Location_data)
 
 # Summarize climate variable to calculate spatially-varying climate normals
 PJdata_space <- PJdata %>% 
@@ -41,16 +44,21 @@ PJdataRAP.scaled <- PJdataRAP %>% mutate_at(scale, .vars = vars(Heatload,PPT_mea
 
 PJdata.scaled <- na.omit(PJdata.scaled)
 
-PJdata.scaled <- subset(PJdata.scaled,Year_t == 2000)
-
+#PJdata.scaled <- subset(PJdata.scaled,Year_t == 2000)
 
 # Prep data for stan model
 x <- as.matrix(select(PJdata.scaled,Heatload,PPT_mean,Tmin_mean,Tmax_mean,PPT_dev,Tmin_dev,Tmax_dev))
-pc_t <- as.vector(PJdata.scaled$PC_t)
-pc_t1 <- as.vector(PJdata.scaled$PC_t1) # need to combine this with pc_t in a single object
-
-K <- ncol(x)
 n <- nrow(x)
+K <- ncol(x)
+
+pc <- as.vector(c(PJdata.scaled$PC_t,PJdata.scaled$PC_t1[which(PJdata.scaled$Year_t==2015)]))
+year <- as.vector(c(PJdata.scaled$Year_t,PJdata.scaled$Year_t1[which(PJdata.scaled$Year_t==2015)]))
+year_ID=as.numeric(factor(year, levels = unique(sort(year))))
+pixel <- as.vector(c(PJdata.scaled$Pixel_ID,PJdata.scaled$Pixel_ID[which(PJdata.scaled$Year_t==2015)]))
+
+n_pc <- length(pc)
+n_tot <- nrow(pc_mat)
+n_year <- ncol(pc_mat)
 
 rmse <- 9.6
 rmse_rap <- 8.77
@@ -61,28 +69,30 @@ sink("pjcover.stan")
 cat("
     data {
     
-    int<lower=0> K;           // N. predictors 
-    int<lower=0> n;           // N. observations
-    matrix[n,K] x;            // Predictor matrix
-    vector<lower=0>[n] pc_t;           // percent cover at time t (not RAP)
-    //vector[n] pc_t_r;         // percent cover at time t (RAP)
-    vector<lower=0>[n] pc_t1;              // percent cover at time t+1 
-    //vector[n] pc_t1_r;         // percent cover at time t+1 (RAP)
-    real<lower=0> sigma_pc;      // rmse of pc cover estimates
+    //int<lower=0> K;                // N. predictors 
+    //int<lower=0> n;                // N. observations
+    int<lower=0> n_pc;                // N. observations
+    int<lower=0> n_tot;            // Total number of pixels in percent cover raster
+    int<lower=0> n_year;           // Number of years of data
+    //matrix[n,K] x;                 // Predictor matrix
+    vector<lower=0>[n_pc] pc;         // percent cover (not RAP)
+    real[n_pc] year;       // vector of year indices for percent cover data
+    real[n_pc] pixel;      // vector of pixel indices for percent cover data
+    //vector[n] pc_t_r;              // percent cover (RAP)
+    real<lower=0> sigma_pc;        // rmse of pc cover estimates
 
     }
     
     parameters {
     
     real u_beta0;                          // intercept means
-    vector[K] u_beta;                      // other coeff mean
+    //vector[K] u_beta;                      // other coeff mean
     real u_beta_pc;                          // intercept means
 
     real<lower=0> sigma_y;                 // residual variation
     
-    vector[n] log_pc_t;
-    vector[n] log_pc_t1;
-    
+    matrix[n_tot,n_year] log_pc;
+
     //vector<lower=0>[n] pc_true;                     // true, unobserved percent cover at time t
 		//vector<lower=0>[n] pc1_true;                    // true, unobserved percent cover at time t+1
 
@@ -94,19 +104,24 @@ cat("
     model {
 
     u_beta0 ~ normal(0, 100);
-    u_beta ~ normal(0, 100); 
+    //u_beta ~ normal(0, 100); 
     u_beta_pc ~ normal(0, 100); 
     
     sigma_y ~ cauchy(0,5);
 
-    pc_t ~ normal(exp(log_pc_t),sigma_pc);
-    pc_t1 ~ normal(exp(log_pc_t1),sigma_pc);
-    
+		for(i in 1:n_tot){
+			for(y in 1:n_year){
+				pc[which(pixel==i & year==y)] ~ normal(exp(log_pc[1,y]),sigma_pc);
+			}
+		}
 
     // Percent Cover Model
     
-    log_pc_t1 - log_pc_t ~ normal(u_beta0 + x*u_beta + u_beta_pc*exp(log_pc_t),sigma_y);
+    for(t in 1:16){
+        log_pc[,t+1] - log_pc[,t] ~ normal(u_beta0 + u_beta_pc*exp(log_pc[,t]),sigma_y);
+    }
     
+    //+ x*u_beta 
     }
     
    
@@ -115,7 +130,7 @@ cat("
 sink()
 
 
-pj_data <- list(K=K, n = n, x=x, pc_t = pc_t, pc_t1 = pc_t1, sigma_pc = rmse)
+pj_data <- list(n = n, n_tot = n_tot, n_year = n_year, pc = pc, year = year_ID, pixel = pixel, sigma_pc = rmse)
 
 rm(PJdata,PJdata_space)
 
