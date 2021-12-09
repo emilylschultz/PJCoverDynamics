951fea4d012480a0ac908763d8c432ab56d8a71d
Emily <emilylschultz@gmail.com>
2021-12-09 09:11:55 -0800

    Add functions to create diagnostic plots of Bayesian model output

--- Code/Plots/PJ_SamplerPlots.R
+++ Code/Plots/PJ_SamplerPlots.R
@@ -1,25 +1,64 @@
+library(ggplot2)
 
-meanBeta <- colMeans(betaOut_ddint)
-sdBeta <- apply(betaOut,2,sd)
+# beta: smaller observation error
+# beta2: original values
+# beta3: q and r = 2
 
-meanLatent <- apply(latOut,c(1,2),mean)
+meanBeta <- colMeans(betaOut,na.rm=T)
+sdBeta <- apply(betaOut,2,sd,na.rm=T)
+name_vec <- c("Intercept","heatload","Mean PPT","Min T","Max T","PPT dev","Tmin dev","Tmax dev",
+              "heatload*PPT","heatload*Tmin","heatload*Tmax","heatload*PPTdev","heatload*Tmindev","heatload*Tmaxdev",
+              "PPT*Tmin","PPT*Tmax","Tmin*Tmax","PPTmean*PPTdev","Tminmean*Tmindev","Tmaxmean*Tmaxdev")
+coef_dat <- data.frame(Names=name_vec,mean=meanBeta,sd_plus=meanBeta+sdBeta,sd_minus=meanBeta-sdBeta)
 
-plot(1:37,meanLatent[,1],type="l")
-lines(1:37,Y1[,latentkeep[1]],type="l",col="lightblue")
-lines(1:37,Y2[,latentkeep[1]],type="l",col="blue")
+ggplot(coef_dat, aes(x = Names, y = mean)) +
+  geom_hline(yintercept = 0, colour = gray(1/2), lty = 2) +
+  geom_point() + 
+  geom_linerange(aes(x = Names, 
+                     ymin = sd_minus,
+                     ymax = sd_plus),
+                 lwd = 1/2) + 
+  coord_flip()
 
-plot(1:37,meanLatent[,2],type="l",ylim=c(-20,5))
-lines(1:37,Y1[,latentkeep[2]],type="l",col="lightblue")
-lines(1:37,Y2[,latentkeep[2]],type="l",col="blue")
+meanLatent <- apply(latOut,c(1,2),mean,na.rm=T)
 
-plot(1:37,meanLatent[,3],type="l",ylim=c(-20,5))
-lines(1:37,Y1[,latentkeep[3]],type="l",col="lightblue")
-lines(1:37,Y2[,latentkeep[3]],type="l",col="blue")
+latent_plot <- function(meanLatent,latentkeep,N){
+  # Add confidence intervals to latent states
+  # Add confidence intervals to observations (constant)
+  sample <- sample(latentkeep,size=N)
+  for(i in sample){
+    plot(1:37,exp(meanLatent[,which(latentkeep==i)]),type="l",ylim=c(0,50))
+    lines(1:37,exp(Y1[,i]),type="l",col="lightblue")
+    lines(1:37,exp(Y2[,i]),type="l",col="blue")
+  }
+}
 
-plot(1:37,meanLatent[,4],type="l",ylim=c(0,5))
-lines(1:37,Y1[,latentkeep[4]],type="l",col="lightblue")
-lines(1:37,Y2[,latentkeep[4]],type="l",col="blue")
+latent_plot(meanLatent,latentkeep,10)
 
-plot(1:37,meanLatent[,5],type="l",ylim=c(0,5))
-lines(1:37,Y1[,latentkeep[5]],type="l",col="lightblue")
-lines(1:37,Y2[,latentkeep[5]],type="l",col="blue")
+beta_chain <- function(beta,start,end,names){
+  for(i in 1:dim(beta)[2]){
+    plot(start:end,beta[start:end,i],type="l",xlab="Iteration",ylab=names[i])
+         #ylim=c(min(beta[start:end,i],na.rm=T),max(beta[start:end,i],na.rm=T)))
+    #lines(start:end,beta2[start:end,i],type="l",xlab="Iteration",ylab=names[i],col="blue")
+    #lines(start:end,beta3[start:end,i],type="l",xlab="Iteration",ylab=names[i],col="green")
+  }
+}
+
+beta_chain(betaOut,1,50000,name_vec)
+
+plot(25000:50000,procVout[25000:50000],type="l")
+plot(25000:50000,sampVout[25000:50000,1],type="l")
+plot(25000:50000,sampVout[25000:50000,2],type="l")
+hist(sampVout[25000:50000,1])
+hist(sampVout[25000:50000,2])
+
+latent_chain <- function(latent,year,start,end,N){
+  sample <- sample.int(300,size=N)
+  for(i in sample){
+    plot(start:end,latOut[year,i,start:end],type="l")
+  }
+  
+}
+
+latent_chain(latOut,25,25000,50000,5)
+
