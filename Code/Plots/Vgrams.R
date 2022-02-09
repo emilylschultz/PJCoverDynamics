16212511aabebb13c532b9fcf237d67f44ee330b
Emily <emilylschultz@gmail.com>
2022-02-09 11:44:03 -0800

    Add script to make variograms

---
+++ Code/Plots/Vgrams.R
@@ -0,0 +1,185 @@
+## Variogram
+library(sp)
+library(gstat)
+
+load("locations.rda")
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[1,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[2,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[3,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[4,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[5,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[6,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[7,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[8,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[9,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[10,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[11,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[12,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[13,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[14,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[15,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[16,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[17,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[18,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[19,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[20,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[21,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[22,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[23,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[24,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[25,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[26,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[27,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[28,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[29,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[30,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[31,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[32,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[33,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[34,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[35,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
+
+Var.Data <- data.frame(x=location.x[sample],y=location.y[sample],resid=resid[36,])
+coordinates(Var.Data)= ~ x+y
+TheVariogram=variogram(resid~1, data=Var.Data)
+plot(TheVariogram)
